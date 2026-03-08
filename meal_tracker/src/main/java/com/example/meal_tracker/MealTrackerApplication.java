9c68595e0c1400627d47205bfacb6b92416b773d
KJ OBrien <kjokjo219@gmail.com>
2026-03-07 19:12:09 -0500

    Initial spring boot commit

---
+++ meal_tracker/src/main/java/com/example/meal_tracker/MealTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.meal_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealTrackerApplication.class, args);
+	}
+
+}
