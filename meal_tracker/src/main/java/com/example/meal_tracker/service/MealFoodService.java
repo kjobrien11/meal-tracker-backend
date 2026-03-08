7112680b4dbf9785db5fadf8481256b7b9bb3d76
KJ OBrien <kjokjo219@gmail.com>
2026-03-08 14:10:04 -0400

    cleans up unused imports

--- meal_tracker/src/main/java/com/example/meal_tracker/service/MealFoodService.java
+++ meal_tracker/src/main/java/com/example/meal_tracker/service/MealFoodService.java
@@ -1,10 +1,7 @@
 package com.example.meal_tracker.service;
 
 import com.example.meal_tracker.dto.MealFoodDTO;
-import com.example.meal_tracker.model.Meal;
 import com.example.meal_tracker.model.MealFood;
-
-import java.util.List;
 
 public interface MealFoodService {
     public MealFoodDTO findByMealId(Integer mealId);
